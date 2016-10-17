028a56097af270a1894675e3d6a5840d1649b096
weizsw <weizsw888@gmail.com>
2016-10-17 14:56:09 -0500

    Add files via upload

---
+++ input_error.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   input_error.h
+ * Author: Daredo
+ *
+ * Created on 2016年10月17日, 下午2:52
+ */
+
+#ifndef INPUT_ERROR_H
+#define INPUT_ERROR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INPUT_ERROR_H */
+
---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Daredo
+ *
+ * Created on 2016年10月17日, 下午2:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
